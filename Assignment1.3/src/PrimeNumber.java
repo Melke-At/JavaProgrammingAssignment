9a1064d65ba4229057f311dabd39165b7fef6489
Melkamu <melkemech@gmail.com>
2025-08-25 20:40:36 +0300

    All done

---
+++ Assignment1.3/src/PrimeNumber.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class PrimeNumber
+ {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int start;
+        int end;
+
+        do {
+            System.out.print("Enter two numbers: ");
+
+            start = Integer.parseInt(input.nextLine());
+            end = Integer.parseInt(input.nextLine());
+        }
+        while(start > end);
+
+        for (int i = start; i <= end; i++) {
+            boolean isPrime = true;
+
+            if (i < 2) {
+                isPrime = false;
+
+            }
+            else {
+                for (int j = 2; j <= Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
