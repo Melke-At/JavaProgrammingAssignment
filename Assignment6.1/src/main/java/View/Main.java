9bfd3747ae9a71dd988cf7dd9aee7e3d2f7f2974
Melkamu <melkemech@gmail.com>
2025-10-05 20:05:11 +0300

    Main class added

---
+++ Assignment6.1/src/main/java/View/Main.java
@@ -0,0 +1,14 @@
+package View;
+
+import Controller.DictionaryController;
+import Model.Dictionary;
+import View.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(DictionaryView.class);
+    }
+}
